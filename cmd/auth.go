5ed6fa7a0cbb3ae569add7c4f22927de09ac598f
shiftwave <dev@shiftwave.me>
2025-07-19 23:11:48 +0200

    feat: add func GetAccessToken && add new value keyring token_expiry
    - Add func to check expired token.

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,14 +1,29 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"time"
+
 	"github.com/spf13/cobra"
 	"github.com/zalando/go-keyring"
-	"log"
-	"regexp"
 )
 
 const service string = "42-cli"
+
+type TokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+	Scope       string `json:"scope"`
+	CreatedAt   int64  `json:"created_at"`
+}
 
 var authCmd = &cobra.Command{
 	Use:   "auth",
@@ -95,4 +110,87 @@ func registerKeyring(login42 string, clientUid string, clientSecret string) {
 func unregisterKeyring() {
 	err := keyring.DeleteAll(service)
 	checkError(err)
+}
+
+func GetAccessToken() (string, error) {
+	token, err := getValidToken()
+	if err == nil && token != "" {
+		return token, nil
+	}
+
+	return generateNewToken()
+}
+
+func getValidToken() (string, error) {
+	token, err := keyring.Get(service, "access_token")
+	if err != nil {
+		return "", err
+	}
+
+	expiryStr, err := keyring.Get(service, "token_expiry")
+	if err != nil {
+		return "", err
+	}
+
+	expiry, err := strconv.ParseInt(expiryStr, 10, 64)
+	if err != nil {
+		return "", err
+	}
+
+	if time.Now().Unix() >= expiry {
+		return "", fmt.Errorf("token expired")
+	}
+
+	return token, nil
+}
+
+func generateNewToken() (string, error) {
+	clientID, err := keyring.Get(service, "client_uid")
+	if err != nil {
+		return "", fmt.Errorf("client ID not found in keyring")
+	}
+
+	clientSecret, err := keyring.Get(service, "client_secret")
+	if err != nil {
+		return "", fmt.Errorf("client secret not found in keyring")
+	}
+
+	data := url.Values{}
+	data.Set("grant_type", "client_credentials")
+	data.Set("client_id", clientID)
+	data.Set("client_secret", clientSecret)
+
+	client := &http.Client{Timeout: 30 * time.Second}
+	resp, err := client.PostForm("https://api.intra.42.fr/oauth/token", data)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("authentication failed with status: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var tokenResp TokenResponse
+	if err := json.Unmarshal(body, &tokenResp); err != nil {
+		return "", err
+	}
+
+	expiry := time.Now().Unix() + int64(tokenResp.ExpiresIn) - 300
+	err = keyring.Set(service, "access_token", tokenResp.AccessToken)
+	if err != nil {
+		return "", err
+	}
+
+	err = keyring.Set(service, "token_expiry", strconv.FormatInt(expiry, 10))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenResp.AccessToken, nil
 }
