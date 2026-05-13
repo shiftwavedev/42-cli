c7af6d69a289b7897ba79116c9d84125f866ae13
shiftwavedev <dev@shiftwave.me>
2026-05-13 16:50:34 +0200

    chore: add note about --web support all intranet vers

--- pkg/display/browser.go
+++ pkg/display/browser.go
@@ -33,6 +33,7 @@ func (IntraURL) User(login string) string {
 // Projects returns the projects URL
 // For the current user: https://projects.intra.42.fr/
 // For other users: https://projects.intra.42.fr/projects/graph?login=<user>
+// NOTE: --web is currently focused on the 42 Central/Paris system and does not yet support other etablissements, which may require a different format :'(
 // TODO: maybe ask user if they use version v2 or v3 of intranet?
 func (IntraURL) Projects(login, currentUserLogin string) string {
 	if login == currentUserLogin {
